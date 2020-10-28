31d8f21d590dbe98994f0e4de9eb386c4082b6b1
Triansh <trianshsharma@gmail.com>
2020-10-28 17:50:29 +0530

    Changed makefile and added time command

---
+++ time.c
@@ -0,0 +1,29 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char * argv[]){
+
+    if(argc < 2){
+        printf(2,"time: Too less arguments. Expecting atleast one argument.\n");
+        exit();
+    }
+
+    int pid = fork();
+
+    if(pid < 0){
+        printf(2, "time: Error in creating process.\n");
+        exit();
+    } else if (pid == 0){
+        int ret = exec(argv[1], argv + 1);
+        if(ret < 0) {
+            printf(2, "exec: Error in running command.\n");
+            exit();
+        }
+    } else {
+        int wtime, rtime;
+        waitx(&wtime, &rtime);
+        printf(1, "The process with pid %d ran for %d and waited for %d. \n", pid, rtime, wtime);
+    }
+    exit();
+}
